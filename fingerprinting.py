834cc2c2fec09ccf2e65d3ac5dc512104b7f6415
James Campbell <james.campbell@tanti.org.uk>
2016-11-21 21:56:37 +0000

    remove use of os

--- fingerprinting.py
+++ fingerprinting.py
@@ -1,9 +1,9 @@
 import sys
-import os
+# import os
 import csv
 import axelrod as axl
 
-user = os.getlogin()
+# user = os.getlogin()
 turns = int(sys.argv[1])
 repetitions = [5, 10, 50, 100]
 probes = [axl.TitForTat, axl.TitFor2Tats, axl.TwoTitsForTat, axl.Bully,
@@ -23,40 +23,40 @@ def write_to_file(data, directory):
 
 
 if __name__ == "__main__":
-    if user == 'James':
-        for rep in repetitions[:1]:
-            for probe in probes[:1]:
-                for strategy in axl.strategies[:1]:
-                    af = axl.AshlockFingerprint(strategy, probe)
-                    data = af.fingerprint(turns=turns, repetitions=rep, step=0.01)
-                    for cmap in col_maps:
-                        for intpl in interpolations:
-                            for ftype in file_types:
-                                p = af.plot(col_map=cmap, interpolation=intpl)
-                                directory = '/Users/James/Projects/fingerprints/images/'
-                                directory += '{}/{}/{}/'.format(turns, rep, probe.__name__)
-                                write_to_file(data, directory)
-                                directory += '{}/{}/'.format(cmap, intpl)
-                                if not os.path.exists(directory):
-                                    os.makedirs(directory)
-                                directory += '{}.{}'.format(strategy.__name__, ftype)
-                                p.savefig(directory)
+    # if user == 'James':
+    #     for rep in repetitions[:1]:
+    #         for probe in probes[:1]:
+    #             for strategy in axl.strategies[:1]:
+    #                 af = axl.AshlockFingerprint(strategy, probe)
+    #                 data = af.fingerprint(turns=turns, repetitions=rep, step=0.01)
+    #                 for cmap in col_maps:
+    #                     for intpl in interpolations:
+    #                         for ftype in file_types:
+    #                             p = af.plot(col_map=cmap, interpolation=intpl)
+    #                             directory = '/Users/James/Projects/fingerprints/images/'
+    #                             directory += '{}/{}/{}/'.format(turns, rep, probe.__name__)
+    #                             write_to_file(data, directory)
+    #                             directory += '{}/{}/'.format(cmap, intpl)
+    #                             if not os.path.exists(directory):
+    #                                 os.makedirs(directory)
+    #                             directory += '{}.{}'.format(strategy.__name__, ftype)
+    #                             p.savefig(directory)
 
-    if user == 'c1304586':
-        for rep in repetitions:
-            for probe in probes:
-                for strategy in axl.strategies:
-                    af = axl.AshlockFingerprint(strategy, probe)
-                    data = af.fingerprint(turns=turns, repetitions=rep, step=0.01)
-                    for cmap in col_maps:
-                        for intpl in interpolations:
-                            for ftype in file_types:
-                                p = af.plot(col_map=cmap, interpolation=intpl, processes=0)
-                                directory = '/scratch/c1304586/fingerprints/images/'
-                                directory += '{}/{}/{}/'.format(turns, rep, probe.__name__)
-                                write_to_file(data, directory)
-                                directory += '{}/{}/'.format(cmap, intpl)
-                                if not os.path.exists(directory):
-                                    os.makedirs(directory)
-                                directory += '{}.{}'.format(strategy.__name__, ftype)
-                                p.savefig(directory)
+    # if user == 'c1304586':
+    for rep in repetitions:
+        for probe in probes:
+            for strategy in axl.strategies:
+                af = axl.AshlockFingerprint(strategy, probe)
+                data = af.fingerprint(turns=turns, repetitions=rep, step=0.01)
+                for cmap in col_maps:
+                    for intpl in interpolations:
+                        for ftype in file_types:
+                            p = af.plot(col_map=cmap, interpolation=intpl, processes=0)
+                            directory = '/scratch/c1304586/fingerprints/images/'
+                            directory += '{}/{}/{}/'.format(turns, rep, probe.__name__)
+                            write_to_file(data, directory)
+                            directory += '{}/{}/'.format(cmap, intpl)
+                            if not os.path.exists(directory):
+                                os.makedirs(directory)
+                            directory += '{}.{}'.format(strategy.__name__, ftype)
+                            p.savefig(directory)
