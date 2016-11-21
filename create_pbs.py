aa55a8ac195456fb7548e263e3c6a4d84c491cc3
James Campbell <james.campbell@tanti.org.uk>
2016-11-21 21:40:11 +0000

    prevent matplotlib from opening stuff up

--- create_pbs.py
+++ create_pbs.py
@@ -9,6 +9,8 @@ command = """#!/bin/bash
 #PBS -l walltime=60:00:00
 #PBS -P PR350
 
+export MPLBACKEND="agg"
+
 /home/c1304586/anaconda3/bin/python /home/c1304586/fingerprints/fingerprinting.py {}
 """
 
